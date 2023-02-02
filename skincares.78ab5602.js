06ac5f54d757e413d66d403372c17ff4f5b151c7
mrklouie <mark.17dullavin@gmail.com>
2023-02-02 08:41:10 +0800

    Feb-2-2023
    Desktop version is done, only mobile version is left

---
+++ skincares.78ab5602.js
@@ -0,0 +1,2 @@
+const navbar=document.querySelector(".navbar"),navbarHeight=navbar.getBoundingClientRect().height,containerEl=document.querySelector("#skincare-page .container");containerEl.style.paddingTop=`${navbarHeight}px`;
+//# sourceMappingURL=skincares.78ab5602.js.map
